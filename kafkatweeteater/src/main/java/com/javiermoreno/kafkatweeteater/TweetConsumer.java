4747a63285ceb0255e60b191c107ac4defab46e9
unknown <Javier@W001037.int.capside.loc>
2015-10-07 22:34:50 +0200

    Agregado ejemplo kafka.

---
+++ kafkatweeteater/src/main/java/com/javiermoreno/kafkatweeteater/TweetConsumer.java
@@ -0,0 +1,35 @@
+package com.javiermoreno.kafkatweeteater;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
+/**
+ *
+ * @author ciberado
+ */
+public class TweetConsumer {
+
+    private final ConsumerConnector consumer;
+    private final String topic;
+
+    public TweetConsumer(String topic) {
+    }
+
+
+    public void ejecutar() {
+    }
+
+    public void apagar() {
+    }
+    
+    public static void main(String[] args) {
+    }
+
+}
---
+++ kafkatweeteater/src/main/java/com/javiermoreno/kafkatweeteater/TweetProducer.java
@@ -0,0 +1,21 @@
+package com.javiermoreno.kafkatweeteater;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+/**
+ *
+ * @author ciberado
+ */
+public class TweetProducer {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
